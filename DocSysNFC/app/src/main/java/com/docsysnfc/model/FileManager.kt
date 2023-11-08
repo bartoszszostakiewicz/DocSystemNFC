d4dd420fcbe1fc5923a14ad2e3af282c4d810183
bartek64377 <79254888+bartek64377@users.noreply.github.com>
2023-11-08 19:41:25 +0100

    adding file from device

---
+++ DocSysNFC/app/src/main/java/com/docsysnfc/model/FileManager.kt
@@ -0,0 +1,94 @@
+package com.docsysnfc.model
+
+import android.app.Activity
+import android.content.Intent
+import android.icu.text.DecimalFormat
+import android.net.Uri
+import android.provider.OpenableColumns
+import androidx.activity.result.ActivityResultLauncher
+
+
+class FileManager(
+    //private val securityManager: SecurityManager
+) {
+
+
+    private val selectedFiles = mutableListOf<Uri>()
+
+
+    fun addFile(uri: Uri) {
+        selectedFiles.add(uri)
+    }
+
+    fun getFiles(): MutableList<Uri> {
+        return selectedFiles
+    }
+
+
+    /**
+     * @param activity
+     * @param launcher
+     * @return uri file
+     */
+
+    fun chooseFile(activity: Activity, launcher: ActivityResultLauncher<Intent>) {
+        val intent = Intent(Intent.ACTION_GET_CONTENT)
+        intent.type = "*/*"
+        launcher.launch(intent)
+    }
+
+    /**
+     * @param context
+     * @param uri
+     * @return size file in bytes
+     */
+
+    fun getSizeFile(context: Activity, uri: Uri): Double {
+        val contentResolver = context.contentResolver
+        val fileDescriptor = contentResolver.openFileDescriptor(uri, "r")
+        val sizeInBytes = fileDescriptor?.use {
+            it.statSize.toDouble()
+        } ?: 0.0 // Jeśli plik nie istnieje lub nie można go odczytać, zwróć 0.0 MB
+
+        // Przelicz na megabajty (MB) i zaokrąglij do 2 miejsc po przecinku
+        val sizeInMB = sizeInBytes / (1000.0 * 1000.0)
+        return DecimalFormat("#.##").format(sizeInMB).toDouble()
+    }
+
+    /**
+     * @param context
+     * @param uri
+     * @return name file
+     */
+
+    fun getNameFile(context: Activity, uri: Uri, extension: Boolean): String {
+        val contentResolver = context.contentResolver
+        val cursor = contentResolver.query(uri, null, null, null, null)
+        val nameIndex = cursor?.getColumnIndex(OpenableColumns.DISPLAY_NAME)
+        cursor?.moveToFirst()
+        val name = cursor?.getString(nameIndex!!)
+        cursor?.close()
+        if (name != null) {
+
+            if (extension) {
+                return name.toString()
+            }
+
+            return name.toString().substringBeforeLast(".").substring(0, minOf(name.length, 10))
+        } else {
+            return "default"
+        }
+
+    }
+
+    /**
+     * @param context
+     * @param uri
+     * @return type file
+     */
+
+    fun getTypeFile(context: Activity, uri: Uri): String {
+        return getNameFile(context, uri, extension = true).substringAfterLast(".")
+    }
+
+}
---
+++ DocSysNFC/app/src/main/java/com/docsysnfc/model/SecurityManager.kt
@@ -0,0 +1,6 @@
+package com.docsysnfc.model
+
+class SecurityManager {
+
+
+}
